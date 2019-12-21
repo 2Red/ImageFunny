d38137a342734f0f9a28c912e3be5d3660980c80
Nguyen Truong An <annguyen463@gmail.com>
2019-12-21 14:25:12 +0700

    Admin (#1)
    * Implement admin login: add passport, session, define user model, write api
    
    * Update index

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const validator = require('express-validator');
+
+const User = require('./models/user');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -12,10 +17,34 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/ImageFunny', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log('Connected!');
+    User.findOne({ 'username': 'admin' }, function (err, user) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        if (user) {
+            console.log('Connected!');
+            return;
+        }
+
+        let userAdmin = new User();
+        userAdmin.username = 'admin';
+        userAdmin.password = userAdmin.encryptPassword('admin');
+        userAdmin.save(function (err, result) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Admin is created!!!');
+            return;
+        });
+    });
 }).catch(err => {
     console('Connect failed!');
 });
+
+require('./config/passport');
 
 app.use(cors({
     origin: 'http://localhost:3000'
@@ -23,7 +52,11 @@ app.use(cors({
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({ secret: 'mysupersecret', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+userSchema.methods.encryptPassword = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+};
+
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
